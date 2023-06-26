d85d945293b0f95e3a5bfc88ac139cfc13c9d6ef
Finn Kumkar <finn.kumkar@gmail.com>
2023-06-26 14:19:35 +0200

    Add OpenBLAS workaround for Windows

--- rebuild_llama.cpp.ps1
+++ rebuild_llama.cpp.ps1
@@ -1,3 +1,36 @@
+if (-not(Test-Path -Path "./vendor/OpenBLAS/OpenBLAS.zip")) {
+
+    Invoke-WebRequest `
+        -Uri "https://github.com/xianyi/OpenBLAS/releases/download/v0.3.23/OpenBLAS-0.3.23-x64.zip" `
+        -OutFile "./vendor/OpenBLAS/OpenBLAS.zip"
+
+    Expand-Archive `
+        -Path "./vendor/OpenBLAS/OpenBLAS-0.3.23-x64.zip" `
+        -DestinationPath "./vendor/OpenBLAS" `
+        -Force
+}
+
+function Resolve-UnixPath {
+
+    Param (
+        [String] $path
+    )
+
+    Write-Output ((Resolve-Path "$path").Path -replace '\\','/')
+}
+
+$lines = @(
+    "# This is a workaround for https://github.com/ggerganov/llama.cpp/issues/627"
+    "include_directories(`"$(Resolve-UnixPath "./vendor/OpenBLAS/include")`")"
+    "add_link_options(`"$(Resolve-UnixPath "./vendor/OpenBLAS/lib/libopenblas.dll.a")`")"
+    "add_compile_definitions(GGML_USE_OPENBLAS)"
+    ""
+)
+
+if (!(Select-String -Path "./vendor/llama.cpp/CMakeLists.txt" -Pattern $lines[0] -SimpleMatch -Quiet)) {
+    $lines + (Get-Content "./vendor/llama.cpp/CMakeLists.txt") | Set-Content "./vendor/llama.cpp/CMakeLists.txt"
+}
+
 git submodule update --remote --merge
 
 Remove-Item  -Path "./vendor/llama.cpp/build" -Force -Recurse
@@ -8,7 +41,10 @@ Push-Location -Path "./"
 
 Push-Location -Path "./vendor/llama.cpp/build"
 
-cmake .. -DLLAMA_CUBLAS=ON
+cmake .. `
+    -DLLAMA_CUBLAS=ON `
+    -DLLAMA_BLAS=ON `
+    -DLLAMA_BLAS_VENDOR=OpenBLAS
 
 cmake --build . --config Release
 
