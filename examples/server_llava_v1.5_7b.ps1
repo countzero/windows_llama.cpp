4b4821880ff03795be6ce44104d13c597c300edb
Finn Kumkar <finn.kumkar@gmail.com>
2023-10-23 22:43:29 +0200

    Add multimodal server example

--- examples/server_llava_v1.5_7b.ps1
+++ examples/server_llava_v1.5_7b.ps1
@@ -1,8 +1,8 @@
-../vendor/llama.cpp/build/bin/Release/llava `
+Start-Process "http://127.0.0.1:8080"
+
+../vendor/llama.cpp/build/bin/Release/server `
     --model "../vendor/llama.cpp/models/llava-v1.5-7b/ggml-model-q5_k.gguf" `
     --mmproj "../vendor/llama.cpp/models/llava-v1.5-7b/mmproj-model-f16.gguf" `
-    --image "../images/the_ocean_race_flyby_kiel.jpg" `
-    --temp 0.1 `
-    --prompt  "Explain the image. Where could it be?" `
+    --ctx-size 4096 `
     --threads 16 `
     --n-gpu-layers 35
