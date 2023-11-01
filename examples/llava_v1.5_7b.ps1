f7edd8ba2c2ab9c9c9ea06728eab3a691e9c2576
Finn Kumkar <kumkar@stadtwerk.org>
2023-11-01 09:14:37 +0100

    Add multimodal example

--- examples/llava_v1.5_7b.ps1
+++
@@ -1,8 +0,0 @@
-../vendor/llama.cpp/build/bin/Release/llava `
-    --model "../vendor/llama.cpp/models/llava-v1.5-7b/ggml-model-q5_k.gguf" `
-    --mmproj "../vendor/llama.cpp/models/llava-v1.5-7b/mmproj-model-f16.gguf" `
-    --image "../images/the_ocean_race_flyby_kiel.jpg" `
-    --temp 0.1 `
-    --prompt  "Explain the image. Where could it be?" `
-    --threads 16 `
-    --n-gpu-layers 35
